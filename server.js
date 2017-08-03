0271b1796643b0966ae5e8fe400df979479de22c
khalilsl <khaliloxmp@gmail.com>
2017-08-03 23:34:35 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,35 @@
+const express =require('express');
+const hbs =require('hbs');
+const fs =require('fs');
+
+var app=express();
+
+hbs.registerPartials(__dirname+'/views/partials')
+app.set('view engine','hbs');
+app.use(express.static(__dirname+'/public'));
+app.use((req,res,next)=>{
+  var now =new Date().toString();
+  console.log(now+req.method+req.url);
+
+  next();
+});
+app.use((req,res,next)=>{
+  res.render('err.hbs');
+});
+
+app.get('/',(req,res)=>{
+res.render('home.hbs',{
+  pagetitle:'home page',
+  desc:'fuck you',
+  currentYear:new Date().getFullYear()
+})
+});
+
+
+app.get('/about',(req,res)=>{
+  res.render('about.hbs',{
+    pagetitle:'about page',
+    currentYear:new Date().getFullYear()
+});
+});
+app.listen(3000);
